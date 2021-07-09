faadea8f1dadde083cb623ffe554ccd5ad8f8861
unknown <nmarchukov@gmail.com>
2021-07-08 22:03:01 -0700

    reviewed and added comments

--- 001-templates/002-parse-execute/01-stdout/main.go
+++ 001-templates/002-parse-execute/01-stdout/main.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
-	tpl, err := template.ParseFiles("tpl.gohtml")
+	tpl, err := template.ParseFiles("tpl.gohtml") //ParseFiles returns 2 things; a val of type *Template and val of type error. This is why we are able to declare two variables (tpl and err).
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = tpl.Execute(os.Stdout, nil)
+	err = tpl.Execute(os.Stdout, nil) //Execute is a method that has type *Template attached to it. Execute returns an error. Execute takes in a writter and data. Here we are passing in
+	//                                  os.Stdout(a writter interface) and not data(nil). A writter is just a location to where the data is executed to.
 	if err != nil {
 		log.Fatalln(err)
 	}
